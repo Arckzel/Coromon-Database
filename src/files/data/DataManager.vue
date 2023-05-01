89a643b3b370125fe174eb488b5d8b61c852fe27
Jefferson Danilo da Silva Brito <48453882+Arckzel@users.noreply.github.com>
2023-04-30 22:40:04 -0300

    Componente para pegar dados dos jsons adicionado

---
+++ src/files/data/DataManager.vue
@@ -0,0 +1,49 @@
+
+<template>
+    <div>
+        <a href="#" @click="getCoromons">Carregar Coromons</a>
+    </div>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            coromons: null,
+            types: null,
+            titans: null
+        }
+    },
+
+    methods: {        
+        getCoromons(){
+            fetch("./src/files/data/coromon.json")
+            .then((response) => response.json())
+            .then((json) => this.coromons = json);            
+        },
+        
+        getTypes(){
+            fetch("./src/files/data/type.json")
+            .then((response) => response.json())
+            .then((json) => this.types = json); 
+        },
+        
+        getTitans(){
+            fetch("./src/files/data/titan.json")
+            .then((response) => response.json())
+            .then((json) => this.titans = json); 
+        }
+    },
+
+    mounted(){
+        this.getCoromons()
+        this.getTypes()
+        this.getTitans()
+    }
+
+
+}
+</script>
+
+
