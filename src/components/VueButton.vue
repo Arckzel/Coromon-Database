58f9a4d2dbe0a1907a466bfee5a08dd6e8490886
Jefferson Danilo da Silva Brito <48453882+Arckzel@users.noreply.github.com>
2023-05-04 16:35:27 -0300

    Componente VueButton para rotas e links

---
+++ src/components/VueButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <router-link class="button" v-if="type == 'int'" :to="link"><slot></slot></router-link>
+    <a v-else class="button" :href="link"><slot></slot></a>
+</template>
+
+<script>
+export default {
+    props:{
+        link: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+    }
+}
+</script>
+
+<style>
+a.button{
+    padding-block: 0.5rem;
+    padding-inline: 1rem;
+    background-color: lightblue;
+    border-radius: 10px;
+    color: black;
+    margin-inline: 0.5rem;
+    font-weight: bold;
+
+    border-style: solid;
+    border-width: 3px;
+    box-shadow: inset #3a74ec  0px 0px 1px 3px;
+}
+</style>
