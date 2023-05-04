a2ecdd49f40bca819928e886c673a7f4ad62ed2c
Jefferson Danilo da Silva Brito <48453882+Arckzel@users.noreply.github.com>
2023-05-04 02:19:06 -0300

    Menu básico para acessar as rotas - Em testes
    Apenas 1 rota disponível por enquanto

---
+++ src/components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="menu">
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+#menu {
+    text-align: center;
+    margin-block: 1rem;
+}
+
+#menu a{
+    padding-block: 0.5rem;
+    padding-inline: 1rem;
+    background-color: lightblue;
+    border-radius: 10px;
+    color: black;
+    margin-inline: 0.5rem;
+
+    border-style: solid;
+    border-width: 3px;
+    box-shadow: inset #3a74ec  0px 0px 1px 3px;
+}
+
+</style>
