95cb881f1a29fd2b049e1cba6fd2b944adf56668
Jefferson Danilo da Silva Brito <48453882+Arckzel@users.noreply.github.com>
2023-05-05 02:45:00 -0300

    Página de erro 404 adicionada

---
+++ src/Pages/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="not-found">
+        <h1>Sorry! </h1>
+        <h2>Page not found. :{{"("}}</h2>
+    </div>
+</template>
+
+<style>
+
+#not-found {
+    text-align: center;
+    margin-block: 50px;
+}
+
+#not-found h1{
+    text-transform: uppercase;
+    font-weight: bolder;
+    font-size: 30px;
+}
+
+#not-found h2{
+    font-size: 25px;
+}
+
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router"
 import DataGrid from '../Pages/DataGrid.vue'
 import CoromonPage from '../Pages/CoromonPage.vue'
+import NotFound from '../Pages/NotFound.vue'
 
 const routes = [
   { path: '/', component: DataGrid },
   { path: '/coromon/:id', component: CoromonPage },
+  { path: "/:catchAll(.*)", component: NotFound, name: "NotFound"},
 ]
 
 const router = createRouter({
