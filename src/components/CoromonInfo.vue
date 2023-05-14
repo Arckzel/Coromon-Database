ffb16460c86f99fdd18491c588d3710b278ee909
Jefferson Danilo da Silva Brito <48453882+Arckzel@users.noreply.github.com>
2023-05-14 02:22:35 -0300

    CoromonInfo.vue:
    - Buttons added to Right side, with the Coromon info

--- src/components/CoromonInfo.vue
+++ src/components/CoromonInfo.vue
@@ -1,89 +1,101 @@
 <template>
-  <div id="coromon-info">
-    <div id="info-content">
-        <div id="base">
-            <div id="head">
-                <h3>Base Stats</h3>
-            </div>
-            <div id="body">
-                <ul>
-                    <li>
-                        <h4>HP: </h4>
-                        <p>{{ coromon.base_hp }}</p>
-                    </li>
-                    <li>
-                        <h4>SPE: </h4>
-                        <p>{{ coromon.base_spe }}</p>
-                    </li>
-                    <li>
-                        <h4>ATK: </h4>
-                        <p>{{ coromon.base_atk }}</p>
-                    </li>
-                    <li>
-                        <h4>DEF: </h4>
-                        <p>{{ coromon.base_def }}</p>
-                    </li>
-                    <li>
-                        <h4>SP.A</h4>
-                        <p>{{ coromon.base_sp_a }}</p>
-                    </li>
-                    <li>
-                        <h4>SP.D</h4>
-                        <p>{{ coromon.base_sp_a }}</p>
-                    </li>
-                    <li>
-                        <h4>SP</h4>
-                        <p>{{ coromon.base_sp }}</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div id="max">
-            <div id="head">
-                <h3>Max Stats</h3>
-            </div>
-            <div id="body">
-                <ul>
-                    <li>
-                        <h4>HP: </h4>
-                        <p>{{ coromon.max_hp }}</p>
-                    </li>
-                    <li>
-                        <h4>SPE: </h4>
-                        <p>{{ coromon.max_spe }}</p>
-                    </li>
-                    <li>
-                        <h4>ATK: </h4>
-                        <p>{{ coromon.max_atk }}</p>
-                    </li>
-                    <li>
-                        <h4>DEF: </h4>
-                        <p>{{ coromon.max_def }}</p>
-                    </li>
-                    <li>
-                        <h4>SP.A</h4>
-                        <p>{{ coromon.max_sp_a }}</p>
-                    </li>
-                    <li>
-                        <h4>SP.D</h4>
-                        <p>{{ coromon.max_sp_a }}</p>
-                    </li>
-                    <li>
-                        <h4>SP</h4>
-                        <p>{{ coromon.max_sp }}</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <div id="coromon-info">
 
+        <div id="info-content">
+           
+            <div id="base">
+                <div id="head">
+                    <h3>Base Stats</h3>
+                    </div>
+                    <div id="body">
+                        <ul>
+                            <li>
+                                <h4>HP: </h4>
+                                <p>{{ coromon.base_hp }}</p>
+                            </li>
+                            <li>
+                                <h4>SPE: </h4>
+                                <p>{{ coromon.base_spe }}</p>
+                        </li>
+                        <li>
+                            <h4>ATK: </h4>
+                            <p>{{ coromon.base_atk }}</p>
+                        </li>
+                        <li>
+                            <h4>DEF: </h4>
+                            <p>{{ coromon.base_def }}</p>
+                        </li>
+                        <li>
+                            <h4>SP.A</h4>
+                            <p>{{ coromon.base_sp_a }}</p>
+                        </li>
+                        <li>
+                            <h4>SP.D</h4>
+                            <p>{{ coromon.base_sp_a }}</p>
+                        </li>
+                        <li>
+                            <h4>SP</h4>
+                            <p>{{ coromon.base_sp }}</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        
+            <div id="max">
+                <div id="head">
+                    <h3>Max Stats</h3>
+                </div>
+                <div id="body">
+                    <ul>
+                        <li>
+                            <h4>HP: </h4>
+                            <p>{{ coromon.max_hp }}</p>
+                        </li>
+                        <li>
+                            <h4>SPE: </h4>
+                            <p>{{ coromon.max_spe }}</p>
+                        </li>
+                        <li>
+                            <h4>ATK: </h4>
+                            <p>{{ coromon.max_atk }}</p>
+                        </li>
+                        <li>
+                            <h4>DEF: </h4>
+                            <p>{{ coromon.max_def }}</p>
+                        </li>
+                        <li>
+                            <h4>SP.A</h4>
+                            <p>{{ coromon.max_sp_a }}</p>
+                        </li>
+                        <li>
+                            <h4>SP.D</h4>
+                            <p>{{ coromon.max_sp_a }}</p>
+                        </li>
+                        <li>
+                            <h4>SP</h4>
+                            <p>{{ coromon.max_sp }}</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+        
+        <div id="buttons">
+            <VueButtonVue :link="'https://coromon.wiki.gg/wiki/'+coromon.name.toLowerCase()" :type="'ext'">Wiki</VueButtonVue>
+        </div>
 
     </div>
-
-  </div>
 </template>
 
 <script>
+import VueButtonVue from '../components/VueButton.vue'
+
 export default {
+
+    components:{
+        VueButtonVue
+    },
 
     props: {
         coromon: {
@@ -130,7 +142,6 @@ export default {
     }
 
     #coromon-info #info-content ul{
-
         display: grid;
         grid-template-columns: 1fr 1fr 1fr 1fr;
 
@@ -138,7 +149,12 @@ export default {
 
     #coromon-info #info-content ul li{
         display: flex;
+    }
 
+    
+    #coromon-info #buttons{
+        margin-top: 20px;
+        text-align: center;
     }
 
     @media (min-width: 780px){
@@ -148,9 +164,7 @@ export default {
         }
 
         #coromon-info #info-content ul{
-
             grid-template-columns: 1fr 1fr 1fr;
-
         }
         
     }
