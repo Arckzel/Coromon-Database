3427ac3949403084180d13990698d36d1b6d791a
Jefferson Danilo da Silva Brito <48453882+Arckzel@users.noreply.github.com>
2023-05-14 10:35:47 -0300

    DataGrid.vue:
    - Not used methods removed
    - coromon.json link fixed

--- src/Pages/DataGrid.vue
+++ src/Pages/DataGrid.vue
@@ -100,22 +100,10 @@ export default{
 
     methods: {        
         getCoromons(){
-            fetch("./src/files/data/coromon.json")
+            fetch("../src/files/data/coromon.json")
             .then((response) => response.json())
             .then((json) => this.coromons = json);            
         },
-        
-        getTypes(){
-            fetch("./src/files/data/type.json")
-            .then((response) => response.json())
-            .then((json) => this.types = json); 
-        },
-        
-        getTitans(){
-            fetch("./src/files/data/titan.json")
-            .then((response) => response.json())
-            .then((json) => this.titans = json); 
-        }
     },
 
     mounted(){
